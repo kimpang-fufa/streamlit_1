2e948eb64de5429093f05c5a7e6ded6377d307e1
kimpang-fufa <kimpang2020@gmail.com>
2026-08-07 13:06:56 -0700

    Spinner

--- streamlit_1.py
+++ streamlit_1.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import random
+import time
 st.title("Rock Paper Scissors")
 choices = ["Rock", "Paper", "Scissors"]
 col1, col2, col3 = st.columns(3)
@@ -13,8 +14,8 @@ if col3.button("Scissors"):
 
 if user_choice:
   comp_choice = random.choice(choices)
-  st.write(f"You chose: **{user_choice}**")
-  st.write(f"Computer chose: **{comp_choice}**")
+  st.write(f"You chose: {user_choice}")
+  st.write(f"Computer chose: {comp_choice}")
 
   if user_choice == comp_choice:
     st.info("It's a tie!")
@@ -26,3 +27,16 @@ if user_choice:
     st.success("You win!")
   else:
     st.error("You lose!")
+    
+    st.warning("Spin a wheel to win you bums not rock, paper, scissors.")
+    
+    with st.spinner("Spinning the wheel to see if you actually win"):
+        time.sleep(2)
+    
+    spinner_verdict = random.choice(["WIN", "FAILURE"])
+    
+    if spinner_verdict == "WIN":
+        st.success("Yipepepeepepe, you're a winner :3")
+        st.balloons()
+    else:
+        st.error("Of course you lost, you never won in the first place :D")
