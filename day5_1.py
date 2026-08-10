2e3d06f39df2878a0b098c324ab06ce356753b9b
kimpang-fufa <kimpang2020@gmail.com>
2026-08-10 11:47:15 -0700

    Update day5_1.py

--- day5_1.py
+++ day5_1.py
@@ -7,12 +7,43 @@ from groq import Groq
 API_KEY = st.secrets["groq_API_KEY"]
 
 
-client = Groq(api_key=API_KEY)
-#MODEL = "qwen2.5:3b"
+import random
+import time
 MODEL = "llama-3.1-8b-instant"
-question = input("Question: ")
-msg = [{"role":"user", "content" : question}]
-# response = ollama.chat(model=MODEL, messsages=msg)
-response = client.chat.completions.create(model=MODEL, messages=msg)
-# print("Answer:", response["message"]["content"])
-print("Answer: ", response.choices[0].message.content)
+st.write("This is an AI chatbot. Please type your questions.")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Let's start chatting! 👇"}]
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        temp = ollama.chat(model=MODEL, messages=st.session_state.messages)
+        assistant_response = temp["message"]["content"]
+            ]
+        )
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
