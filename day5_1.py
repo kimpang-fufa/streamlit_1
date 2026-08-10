f2db1fd7fc37b6a0820af5f5abd86d3fd2172ded
kimpang-fufa <kimpang2020@gmail.com>
2026-08-10 11:51:57 -0700

    Update day5_1.py

--- day5_1.py
+++ day5_1.py
@@ -33,8 +33,11 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        temp = groq.chat(model=MODEL, messages=st.session_state.messages)
-        assistant_response = temp["message"]["content"]
+        completion = client.chat.completions.create(
+            model=MODEL, 
+            messages=st.session_state.messages
+        )
+        assistant_response = completion.choices[0].message.content
         # Simulate stream of response with milliseconds delay
         for chunk in assistant_response.split():
             full_response += chunk + " "
